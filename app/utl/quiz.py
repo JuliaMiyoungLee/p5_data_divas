00385fb8730f0f5ec7d76e683a9ff45c9c11838c
julia <jlee30@stuy.edu>
2023-06-06 03:28:09 -0400

    working on quiz page

---
+++ app/utl/quiz.py
@@ -0,0 +1,12 @@
+import sqlite3
+DB_FILE = "DB_FILE.db"
+
+def update(keys, values, username):
+    query = "UPDATE users SET "
+    for i in range(len(keys)):
+        query += keys[i] + " = " + values[i] + ","
+    query = query[:-2]
+    query += " WHERE username = " + username + ";"
+    db = sqlite3.connect("DB_FILE.db")
+    c = db.cursor()
+    c.execute(query)
